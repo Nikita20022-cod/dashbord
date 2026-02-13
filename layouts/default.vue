0ac528ab77428837439592279eacda947c178ab5
Nikita Sidorov <grant20022@gmail.com>
2026-02-13 17:44:47 +0500

    new info

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,9 @@
     <LayoutBurgerMenu/>
     <!-- main content -->
     <main class="flex-1 p-6">
+      <p class="text-green-600 text-lg md:text-2xl font-bold mb-5">
+        Source code <a class="text-blue-400" target="_blank" href="https://github.com/Nikita20022-cod/dashbord">https://github.com/Nikita20022-cod/dashbord</a>
+      </p>
       <slot />
     </main>
   </div>
